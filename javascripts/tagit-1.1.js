d8fc2e9981a7c4bd497a79b18129f5d1043869f5
unknown <Nikita Singh@FIR000075358003.ads.mckinsey.com>
2014-06-18 16:54:03 +0530

    rectified delete bug

--- javascripts/tagit-1.1.js
+++ javascripts/tagit-1.1.js
@@ -61,6 +61,23 @@
                                         var selectedDiv = $(this);
                                         if(event.keyCode == 8 || event.keyCode == 46){
                                             selectedDiv.next().focus();
+                                            var tagText = selectedDiv.text();
+                                            var result = tagText.substring(0, tagText.length-1);
+                                            var originalValue = $('#tags-input').val();
+                                            var updatedValue = "";
+
+                                            if(originalValue.indexOf(result) == 0){
+                                                if(originalValue.indexOf(',') < 0){
+                                                    updatedValue = originalValue.replace(result, "");    
+                                                }
+                                                else{
+                                                    updatedValue = originalValue.replace(result + ',', "");    
+                                                }
+                                            }
+                                            else{
+                                                updatedValue = originalValue.replace(',' + result, "");    
+                                            }
+                                            $('#tags-input').val(updatedValue);
                                             selectedDiv.remove();
                                         }
                                         else if(event.keyCode == 38){
